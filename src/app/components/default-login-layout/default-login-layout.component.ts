1297106f2c368c632160fd702d00cd9061cfb600
CaueGrassi <cauegrassi7@gmail.com>
2025-01-26 18:18:26 -0300

    feat: finish mvp

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
+  standalone: true,
   imports: [],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
@@ -9,16 +10,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class DefaultLoginLayoutComponent {
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
-  @Input() secundaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
   @Input() disablePrimaryBtn: boolean = true;
   @Output("submit") onSubmit = new EventEmitter();
 
   @Output("navigate") onNavigate = new EventEmitter();
 
-  submit() {
+  submit(){
     this.onSubmit.emit();
   }
-  navigate() {
+
+  navigate(){
     this.onNavigate.emit();
   }
 }
